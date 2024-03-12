f4dc643d35c58194975087e4ee47a897894fe140
Dzeniks <jan.soukenik@hotmail.com>
2024-03-12 11:11:43 +0100

    Remove non asset files

--- Project_A1/Packages/98231542a8b0e65db4a115744545fdbf17d77371/package/Editor/Settings/MediationUserSettingsKeys.cs
+++
@@ -1,7 +0,0 @@
-namespace Unity.Services.Mediation.Settings.Editor
-{
-    public struct MediationUserSettingsKeys
-    {
-        public const string forceDynamicLinkingKey = "mediation.build.settings.force-dynamic-linking";
-    }
-}
